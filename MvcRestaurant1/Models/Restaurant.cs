aea9db08ba2754c5b481545465ff72b49cea41dc
Salty <vertixo.vider05@gmail.com>
2022-11-03 22:50:38 +0200

    Adding Validation

--- MvcRestaurant1/Models/Restaurant.cs
+++ MvcRestaurant1/Models/Restaurant.cs
@@ -8,7 +8,9 @@ namespace MvcRestaurant1.Models
 	{
 		public int Id { get; set; }
 
+        [StringLength(60, MinimumLength = 3)]
         [Display(Name = "Restaurant Name")]
+        [Required]
         public string Name { get; set; }
 
 		[Display(Name = "Opening Date")]
@@ -16,8 +18,13 @@ namespace MvcRestaurant1.Models
 		public DateTime OpeningDate { get; set;	}
 		public string Food { get; set; }
 
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
         [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [Required]
         public string Rating { get; set; }
     }
 }
