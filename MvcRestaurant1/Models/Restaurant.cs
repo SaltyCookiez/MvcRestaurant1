7567ae02bcaf87c277a1db8f5b601eacaa027f41
Salty <vertixo.vider05@gmail.com>
2022-11-03 22:25:43 +0200

    Controller actions and views

--- MvcRestaurant1/Models/Restaurant.cs
+++ MvcRestaurant1/Models/Restaurant.cs
@@ -1,16 +1,22 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MvcRestaurant1.Models
 {
 	public class Restaurant
 	{
 		public int Id { get; set; }
-		public string Name { get; set; }
 
-		[DataType(DataType.Date)]
+        [Display(Name = "Restaurant Name")]
+        public string Name { get; set; }
+
+		[Display(Name = "Opening Date")]
+        [DataType(DataType.Date)]
 		public DateTime OpeningDate { get; set;	}
 		public string Food { get; set; }
-		public decimal Price { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
 	}
 }
