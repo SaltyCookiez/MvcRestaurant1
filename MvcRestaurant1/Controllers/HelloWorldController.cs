6947824ca44d276461b763bdd87c83d0c4712c83
Morris <Morris@DESKTOP-JC6QQE3>
2022-11-03 21:38:45 +0200

    Add project files.

---
+++ MvcRestaurant1/Controllers/HelloWorldController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcRestaurant1.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        public string Welcome()
+        {
+            return "This is thw Welcome action method...";
+        }
+    }
+}
